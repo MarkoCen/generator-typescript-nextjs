5f01a52f9e11f3343d9e52f8a5840afd7f4c0441
Marko Cen <markocen@gmail.com>
2020-06-01 01:31:08 -0400

    debug

--- generators/app/templates/nextts/server/lib/cache.ts
+++ generators/app/templates/nextts/server/lib/cache.ts
@@ -1,9 +1,9 @@
-import * as LRU from 'lru-cache';
+import LRU from 'lru-cache';
 
 const cachePool = new LRU({
-    max: 150, // maximum 150mb cache size in memory
-    maxAge: 1000 * 60 * 60 * 3, // maximum 3hr cache time
-    stale: false,
+  max: 150, // maximum 150mb cache size in memory
+  maxAge: 1000 * 60 * 60 * 3, // maximum 3hr cache time
+  stale: false,
 });
 
 export default cachePool;
