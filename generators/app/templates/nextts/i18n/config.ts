bd2a0c9481cd2ed4675f9225db2fe7dd9777d1de
Marko Cen <markocen@gmail.com>
2020-06-01 21:56:08 -0400

    update locale config

--- generators/app/templates/nextts/i18n/config.ts
+++ generators/app/templates/nextts/i18n/config.ts
@@ -4,13 +4,15 @@ const isServer = typeof window === 'undefined';
 
 export const defaultLang = 'en';
 
-export const allLangs = ['en'];
+export const allLangs = ['en', 'zh-Hans'];
 
-export const langSelectors = ['English'];
+export const langSelectors = ['English', '中文'];
 
 export const namespaces = ['common'];
 
-export const localePaths = {};
+export const localePaths = {
+  'zh-Hans': 'zh-hans',
+};
 
 export const nextI18Instance = new NextI18Next({
   debug: false,
